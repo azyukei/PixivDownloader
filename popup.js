7b266183a5c3c0c812058d3fbed9759c6d8233f3
Azyukei <azyukei@gmail.com>
2016-03-03 21:28:32 +0800

    popup 目前還弄不好

--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function() {
+	$(".btn_popup").click(function() {
+		chrome.windows.create({ url: "http://www.pixiv.net/setting_profile.php", type: "popup", width: 800, height: 600}, function() {
+			
+		});
+	});
+});
